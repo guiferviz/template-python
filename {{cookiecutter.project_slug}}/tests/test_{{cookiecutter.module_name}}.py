fe38954ceda66d6aaea648543090338fff056a72
guiferviz <guiferviz@gmail.com>
2023-01-09 11:30:30 +0100

    moving module to src directory

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.module_name}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.module_name}}.py
@@ -1,21 +1,5 @@
 """Tests for `{{ cookiecutter.module_name }}` package."""
 
-import pytest
 
-from {{ cookiecutter.module_name }} import {{ cookiecutter.module_name }}
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/johanvergeer/cookiecutter-poetry')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_import():
+    import {{ cookiecutter.module_name }} as {{ cookiecutter.module_name }}
